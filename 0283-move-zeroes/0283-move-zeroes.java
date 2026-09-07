35a01ae8ea9d30b733a4fdba15163f39dbb36c7b
Bura Prem sai <vtu28402@veltech.edu.in>
2026-09-07 12:26:20 +0530

    Time: 2 ms (91.8%), Space: 48 MB (6.86%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,13 +1,19 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int insertPos = 0;
+        int index = 0;
+
+        // Move all non-zero elements forward
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
-                int temp = nums[insertPos];
-                nums[insertPos] = nums[i];
-                nums[i] = temp;
-                insertPos++;
+                nums[index] = nums[i];
+                index++;
             }
+        }
+
+        // Fill the remaining positions with zeros
+        while (index < nums.length) {
+            nums[index] = 0;
+            index++;
         }
     }
 }
